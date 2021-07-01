87bfda847c28b7423a700336fc0c3c2e38fd758d
Bartosz <radu480@o2.pl>
2021-07-01 12:41:31 +0200

    Extended model.

--- models.py
+++ models.py
@@ -8,3 +8,16 @@ class Link:
     def __init__(self, target_node, costs):
         self.target_node = target_node
         self.costs = costs
+
+
+class Vehicle:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+        self.route = []
+
+
+class RoutePoint:
+    def __init__(self, node_name, time):
+        self.node_name = node_name
+        self.time = time
