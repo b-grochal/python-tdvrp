47a50cabc0f84c0f8cb94410108964579970b690
Bartosz <radu480@o2.pl>
2021-07-30 14:47:30 +0200

    Prepared real costs for first nodes.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from controllers import AppController
 def main(arguments):
     app_controller = AppController()
     #app_controller.simulate(arguments[1])
-    app_controller.simulate("nodes.json", "vehicles.json")
+    app_controller.simulate("nodes1.json", "vehicles.json")
 
 
 if __name__ == '__main__':
